ee65da6ff21e9e9e4c58bce5de52d5a11b2094df
siyapareek <93838945+siyapareek@users.noreply.github.com>
2024-01-08 23:07:54 +0530

    Add files via upload

---
+++ Question_39.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Question_39 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int sum=0;
+
+        int n=sc.nextInt();
+        for(int i=0;i<=n;i++){
+             sum=sum+i;
+
+        }
+        System.out.println(sum);
+    }
+}
