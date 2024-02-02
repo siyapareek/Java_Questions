d58b97447c5d6590659263034debd54159509961
siyapareek <93838945+siyapareek@users.noreply.github.com>
2024-02-02 16:49:17 +0530

    Add files via upload

---
+++ Is_the_Score_Consistent.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Is_the_Score_Consistent {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t--!=0){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            int d=sc.nextInt();
+            if(a>c || b>d) System.out.println("IMPOSSIBLE");
+            else System.out.println("POSSIBLE");
+        }
+    }
+}
