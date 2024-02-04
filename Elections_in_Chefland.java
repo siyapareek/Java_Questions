a3727325f4cd2d05a34b6ca2ceb97ba43ff76da2
siyapareek <93838945+siyapareek@users.noreply.github.com>
2024-02-04 14:41:36 +0530

    Add files via upload

---
+++ Elections_in_Chefland.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Elections_in_Chefland {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t--!=0){
+            int n=sc.nextInt();
+            int x=sc.nextInt();
+            int[] arr=new int[n];
+            int count=0;
+            for (int i=0;i<n;i++) arr[i]=sc.nextInt();
+            for (int i=0;i<n;i++) if (arr[i]>=x) count++;
+            System.out.println(count);
+        }
+    }
+}
