7ba339e76104cadb3ac985ba9b9c12ea92f30719
siyapareek <93838945+siyapareek@users.noreply.github.com>
2024-02-19 21:09:41 +0530

    Add files via upload

---
+++ Question_43.java
@@ -0,0 +1,14 @@
+//print array
+public class Question_43 {
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        arr[0]=1;
+        arr[1]=4;
+        arr[2]=7;
+        arr[3]=9;
+        arr[4]=2;
+        System.out.println(arr[1]);
+
+        
+    }
+}
