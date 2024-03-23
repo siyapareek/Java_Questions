b72448ec3fb09abe03690c3724462d176530f8c3
siyapareek <93838945+siyapareek@users.noreply.github.com>
2024-03-23 22:44:23 +0530

    Add files via upload

---
+++ count_forLoop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class count_forLoop {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+
+        for (int num=1;num<=n;num++){
+            System.out.println(num);
+        }
+
+
+    }
+}
